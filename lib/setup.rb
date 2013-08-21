dec23a6cd23b56a53867e5c4253068e583be71eb
Barrett Witkin <bwitkin@gmail.com>
2013-08-21 15:51:31 -0400

    updated Analytics file, corrected errors in other files

--- lib/setup.rb
+++ lib/setup.rb
@@ -1,13 +1,14 @@
+require_relative 'csv_reader'
+require_relative 'area'
+
 class Setup
-  #setup accessable methods/classes and getters/setters
-  require_relative 'csv_reader'
-  require_relative 'area'
   attr_accessor :areas
 
   def initialize
     csv = CSVReader.new("./zipcode-db.csv")
 
     @areas = []
+    # array.each == csv.read
     csv.read do |item|
       @areas << Area.new(item)
     end
@@ -15,19 +16,7 @@ class Setup
 
   end
 
-  def read
-    f = File.new(@fname, 'r')
 
-    # Grab the headers
-    self.headers = f.readline
-  
-    # Loop over the lines
-    while (!f.eof? && next_line = f.readline)
-      values = next_line.split(',')
-      hash = create_hash(values)
-      yield(hash)
-    end
-  end
 
 
 end
